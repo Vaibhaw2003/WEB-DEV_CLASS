53435cca5a8afbb08bb68f18043e9031df2af996
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-10-31 11:09:26 +0530

    new update

---
+++ DAY-10/app.js
@@ -0,0 +1,6 @@
+var a = 10
+export var name = 'HARSH SINGH'
+
+export var age = 21;
+
+export default a;
---
+++ DAY-10/script.js
@@ -0,0 +1,7 @@
+import a from './app.js'
+import { name } from './app.js';  
+import { age } from './app.js';  
+
+console.log(a);
+console.log(age);
+console.log(name);
