941f132b58b0f06a6ccbb532c26173979460f3ef
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-12-02 10:32:03 +0530

    Object syntex extend literals

---
+++ DAY-22/script.js
@@ -0,0 +1,28 @@
+//Object syntex extend literals---------
+
+const name ='vaibhaw singh';
+const age = 22;
+const country = 'India';
+const profession = 'Web Developer';
+const hobby = 'Photography';
+const student = {age, country, profession, hobby};
+console.log(student);
+
+
+//Variable Declaration-------
+
+var x=10;
+let y=20;
+const z=30
+y=25;
+document.body.innerHTML=x;
+document.body.innerHTML+="<br>"+y;
+document.body.innerHTML+="<br>"+z;
+console.log(x,y,z);
+
+//Spread Operator-------
+
+const nums =[1,2,3];
+const newnums=[...nums,4,5];
+console.log(newnums);
+
