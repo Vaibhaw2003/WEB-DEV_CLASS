a2747831ff169c79478cb85d26809b44489b05ed
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-12-03 22:50:50 +0530

    day-23

---
+++ DAY-23/script.js
@@ -0,0 +1,20 @@
+/*
+const greet= () => {
+    console.log("Hello, welcome to Day 23!");
+} 
+
+greet(); */
+
+/*
+const add = (a, b) => {
+    return a + b;
+}
+console.log("Sum of 5 and 3 is: " + add(5, 3));
+*/
+
+
+const square = (num) =>{
+    return num * num;
+}
+console.log("Square of 4 is: " + square(4));
+
