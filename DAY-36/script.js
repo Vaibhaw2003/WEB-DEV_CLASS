35ebb8bec1c6831c57687f09587daa79154021be
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-03-28 23:57:07 +0530

    day-37 done

--- DAY-36/script.js
+++ DAY-36/script.js
@@ -1,5 +1,18 @@
-function send(){
-    console.log("button clicked");
-}
 
-send();
+    document.getElementById("myForm").addEventListener("submit", function(event){
+
+        event.preventDefault(); // Prevent page reload
+
+        let name = document.getElementById("name").value;
+        let email = document.getElementById("email").value;
+        let address = document.getElementById("address").value;
+        let number = document.getElementById("number").value;
+
+        console.log("Name:", name);
+        console.log("Email:", email);
+        console.log("Address:", address);
+        console.log("Number:", number);
+
+        console.log("Form Submitted Successfully 🚀");
+        alert("Submitted Successfully 🚀")
+    });
